5fedcc5f41103d409878edeb4437971ae538f4e9
Daredevil-ii <amrrashad603@gmail.com>
2023-08-15 08:02:33 -0700

    New Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ *
+ * Return: Always 0 (Success)
  */
-int main() {
-    // Write C code here
-    printf("_putchar""\n");
-
-    return 0;
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
