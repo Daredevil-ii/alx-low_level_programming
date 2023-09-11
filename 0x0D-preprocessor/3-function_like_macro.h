0ef155ca8242e8968c0704ebc8eda976e052e372
Daredevil-ii <amrrashad603@gmail.com>
2023-09-11 06:00:24 -0700

    New Commit

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,9 +3,4 @@
 
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-#endif /* _function_like_macro_h_ */#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
-
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-
 #endif /* _function_like_macro_h_ */
